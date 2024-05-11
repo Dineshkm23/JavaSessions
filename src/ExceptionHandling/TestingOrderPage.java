036975abacc736bed6b8d69fb76232ca5ee99cad
Dineshkm23 <83826114+Dineshkm23@users.noreply.github.com>
2024-05-11 16:27:20 +0400

    Create TestingOrderPage.java

---
+++ src/ExceptionHandling/TestingOrderPage.java
@@ -0,0 +1,12 @@
+package ExceptionHandling;
+
+public class TestingOrderPage {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String i= "TestingOrderPage___ Rahul";
+
+	}
+
+}
