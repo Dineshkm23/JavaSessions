403843bfaa4301812f56b0d53805e06f845510f4
Dineshkm23 <83826114+Dineshkm23@users.noreply.github.com>
2024-05-11 16:20:12 +0400

    Create Testingpage.java

---
+++ src/ExceptionHandling/Testingpage.java
@@ -0,0 +1,16 @@
+package ExceptionHandling;
+
+public class Testingpage {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("final key word");
+
+	
+		System.out.println("Testingpage_____ Rahul");
+	);
+
+	}
+
+}
